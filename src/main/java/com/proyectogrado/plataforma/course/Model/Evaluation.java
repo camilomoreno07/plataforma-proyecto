66964fc2f6dd4d12b1f7aaa3b9f67ea8c9c3749e
camilomoreno07 <morenopintocamilo07@gmail.com>
2025-08-25 21:02:44 -0500

    cambio modelo evaluacion

--- src/main/java/com/proyectogrado/plataforma/course/Model/Evaluation.java
+++ src/main/java/com/proyectogrado/plataforma/course/Model/Evaluation.java
@@ -1,14 +1,14 @@
 package com.proyectogrado.plataforma.course.Model;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
 
 @Data
 public class Evaluation {
-
+    private String questionType;   // "OPEN", "MC3", "MC5"
     private String question;
     private String correctAnswer;
     private Integer time;
-
+    private List<String> options;  // null o vacío si es OPEN
 }
